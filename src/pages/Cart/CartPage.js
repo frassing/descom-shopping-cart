926e9d8646e92474b4ae3ab0378b6ee179237e3b
frassing <raquel.f.roessing@hotmail.com>
2023-07-28 19:57:41 -0400

    ajustes cartPage

--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -1,5 +1,7 @@
 import { Container } from "react-bootstrap";
-import { ListGroup } from "../../components/ListGroup/ListGroup";
+import ListGroup from 'react-bootstrap/ListGroup';
+import Image from 'react-bootstrap/Image';
+import { Button } from "../../components/Button/Button";
 import { useAppContext } from "../../store/AppContext";
 import { useEffect } from "react";
 import { fetchCartProcessAction } from "../../store/actions";
@@ -10,23 +12,44 @@ export const CartPage = () => {
 	useEffect(() => {
 		fetchCartProcessAction(dispatch);
 	}, []);
-	
+
 	return (
 		<Container>
 			<h1>Carrinho</h1>
-			<ListGroup 
-				itemsList={state.cart}
-				controls={[
-					{
-						variant: 'dark',
-						label: '+'
-					},
-					{
-						variant: 'dark',
-						label: '-'
-					}
-				]}
-			/>
+			<ListGroup>
+				{state.cart.map((prod, prodIndex) => (
+					<ListGroup.Item as="li" key={prodIndex} className='d-flex flex-direction-column justify-content-between align-items-start text-center'>
+						<Image className="prod_img ms-1 me-1" src={prod.image} rounded style={{ height: '10rem', objectFit: 'cover', width: '10rem' }} />
+							
+						<span className="fw-bold ms-auto me-auto">{prod.name}</span>
+
+						<div className='qtd_btn_prod ms-auto me-auto d-flex-column justify-content-center'>
+							<p className="fw-bold">Quantidade</p>
+							<span className="qtd_value">0</span>
+
+							<div className="buttons pe-1">
+								<span className='plus'>
+									<Button variant={'dark'} label={'+'} className="btn_increment" />
+								</span>
+								<span className='minus ps-1'>
+									<Button variant={'dark'} label={'-'} className="btn_decrement" />
+								</span>
+							</div>
+						</div>
+
+						<div className='valor_unt ms-auto me-auto'>
+							<p className="fw-bold">Valor Unitário</p>
+							<span className="product_price">{prod.price}</span>
+						</div>
+
+						<div className='subtotal_prod ms-auto me-auto'>
+							<p className="fw-bold">Subtotal</p>
+							<span className="subtotal_value">subtotal</span>
+						</div>
+					</ListGroup.Item>
+				))}
+			</ListGroup>
+
 		</Container>
 	);
 }
